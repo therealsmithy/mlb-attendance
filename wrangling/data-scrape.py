8045cd9fd88ee6b1998b603b94e669431fcca024
therealsmithy <liamsmith3434@gmail.com>
2025-02-11 17:04:30 -0500

    Finished scraping baseball cube

--- wrangling/data-scrape.py
+++ wrangling/data-scrape.py
@@ -4,16 +4,40 @@ import requests
 
 years = range(1957, 2025)
 
+# Baseball reference for W/L
 for year in years:
-    url = f'https://www.baseball-reference.com/leagues/majors/{year}.shtml'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find_all('table', {'id': 'expanded_standings_overall'})
-    headers = [th.text.strip() for th in table.find('thead').find_all('th')]
-    rows = []
-    for tr in table.find('tbody').find_all('tr'):
-        cells = [td.text.strip() for td in tr.find_all(['th', 'td'])]
-        rows.append(cells)
-    df = pd.DataFrame(rows, columns=headers)
-    df.to_csv(f'mlb_standings_{year}.csv', index=False)
+    if year == 2020:
+        continue
+    else:
+        url = f'https://www.baseball-reference.com/leagues/majors/{year}.shtml'
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find_all('table', {'id': 'expanded_standings_overall'})
+        headers = [th.text.strip() for th in table.find('thead').find_all('th')]
+        rows = []
+        for tr in table.find('tbody').find_all('tr'):
+            cells = [td.text.strip() for td in tr.find_all(['th', 'td'])]
+            rows.append(cells)
+        df = pd.DataFrame(rows, columns=headers)
+        df.to_csv(f'mlb_standings_{year}.csv', index=False)
+
+# Baseball cube for attendance
+for year in years:
+    if year == 2020:
+        continue
+    else:
+        url = f'https://www.thebaseballcube.com/content/mlb_att_year/{year}/'
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        attendance = soup.select(
+            'tr.header-row.header-grid1.sortrow, .data-row.grid1.row1, .data-row.grid1.row2'
+        )
+        header = [tr.text.strip() for tr in attendance[0]]
+        rows = []
+        for tr in attendance[1:]:
+            cells = [td.text.strip() for td in tr.find_all('td')]
+            rows.append(cells)
+        df = pd.DataFrame(rows, columns=header)
+        df.to_csv(f'mlb_attendance_{year}.csv', index=False)
