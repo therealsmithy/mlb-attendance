1e65e42eb20089015d62d6911010f6260ac4dd69
therealsmithy <liamsmith3434@gmail.com>
2025-02-27 09:25:48 -0500

    Updated models and all vizzes saved

--- work/models.R
+++ work/models.R
@@ -4,6 +4,8 @@ library(fpp3)
 library(zoo)
 library(ggplot2)
 
+# CHANGE VAL TO 3 SO THAT 2021 DOES NOT INFLUENCE
+
 # Read in data
 full <- read.csv('data/full_data.csv')
 cpi <- read.csv('data/cpi.csv')
@@ -103,7 +105,7 @@ pirates$wins_lag <- lag(pirates$wins, 1)
 # FULL LEAGUE MODEL
 # Create time series object
 full_ts <- ts(full_league$attendance, start = 1913, end = 2024)
-nVal <- 5
+nVal <- 3
 nTrain <- length(full_ts) - nVal
 full_train <- window(full_ts, start = 1913, end = c(1913, nTrain))
 full_val <- window(full_ts, start = c(1913, nTrain + 1), end = c(1913, nTrain + nVal))
@@ -114,6 +116,10 @@ summary(rw_full)
 t_stat <- (0.9766 - 1) / 0.0209
 2 * pnorm(t_stat)
 # Random Walk...
+
+# Acf of full league and differenced
+Acf(full_ts)
+Acf(diff(full_ts))
 
 # Plot
 autoplot(full_train) +
@@ -126,53 +132,67 @@ autoplot(full_train) +
 naive_full <- naive(full_train, h = nVal)
 naive_full_forecast <- forecast(naive_full, h = nVal)
 
-# Sarimax model
-sarimax_full <- auto.arima(full_train, xreg = full_league$Avg[1:length(full_train)])
-sarimax_full_forecast <- forecast(sarimax_full, xreg = full_league$Avg[(length(full_train) + 1):(length(full_train) + nVal)])
+# Arimax model
+arimax_full <- auto.arima(full_train, xreg = full_league$Avg[1:length(full_train)])
+arimax_full_forecast <- forecast(arimax_full, xreg = full_league$Avg[(length(full_train) + 1):(length(full_train) + nVal)])
 
-# Sarmiax with lagged predictors
-sarimax_full_lag <- auto.arima(full_train, xreg = full_league$cpi_lag[1:length(full_train)])
-sarimax_full_lag_forecast <- forecast(sarimax_full_lag, xreg = full_league$cpi_lag[(length(full_train) + 1):(length(full_train) + nVal)])
+# Armiax with lagged predictors
+arimax_full_lag <- auto.arima(full_train, xreg = full_league$cpi_lag[1:length(full_train)])
+arimax_full_lag_forecast <- forecast(arimax_full_lag, xreg = full_league$cpi_lag[(length(full_train) + 1):(length(full_train) + nVal)])
 
 # Check accuracy of three models
 accuracy(naive_full_forecast, full_val)
-accuracy(sarimax_full_forecast, full_val)
-accuracy(sarimax_full_lag_forecast, full_val)
-# Naive performs best, which makes sense for a random walk
+accuracy(arimax_full_forecast, full_val)
+accuracy(arimax_full_lag_forecast, full_val)
+# Lagged Arimax actually performs the best!
 
 # Plot three forecasts
 autoplot(full_ts) +
   autolayer(full_train, series = 'Train') +
   autolayer(full_val, series = 'Validation') +
   autolayer(naive_full_forecast, series = 'Naive', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_full_forecast, series = 'Sarimax', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_full_lag_forecast, series = 'Sarimax Lagged', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_full_forecast, series = 'Arimax', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_full_lag_forecast, series = 'Arimax Lagged', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
   ggtitle('Full League Attendance') + 
   theme_minimal()
 
-# Check residuals
-checkresiduals(naive_full)
+# Use lagged arimax model for forecast
+# To do this we have to predict CPI for 2025 to 2027
+# Fit simple linear model to estimate future CPI
+cpi_years <- seq_along(full_league$cpi)
+cpi_model <- lm(full_league$cpi ~ cpi_years)
+cpi_future_years <- (max(cpi_years) + 1):(max(cpi_years) + 3)
+cpi_future <- predict(cpi_model, newdata = data.frame(cpi_years = cpi_future_years))
 
-# Use naive model for forecast
-naive_full_future <- naive(full_ts, h = 5)
-plot(naive_full_future)
+# Forecast
+arimax_full_forecast <- forecast(arimax_full_lag, xreg = cpi_future, h = 3)
+
+# Plot
+autoplot(full_ts) +
+  autolayer(arimax_full_forecast) +
+  xlab('Year') + ylab('Attendance') +
+  ggtitle('Full League Attendance Forecast: ARIMAX with Lagged CPI') +
+  theme_minimal()
 
 # WHITE SOX MODEL
 # Create time series object
 w_sox_ts <-ts(w_sox$attendance, start = 1903, end = 2024)
 nTrain <- length(w_sox_ts) - nVal
-nVal <- 5
+nVal <- 3
 w_sox_train <- window(w_sox_ts, start = 1903, end = c(1903, nTrain))
 w_sox_val <- window(w_sox_ts, start = c(1903, nTrain + 1), end = c(1903, nTrain + nVal))
 
 # Test for Random Walk
 rw_w_sox <- Arima(w_sox_train, order = c(1, 0, 0))
 summary(rw_w_sox)
-t_stat <- (0.8618 - 1) / 0.0524
+t_stat <- (0.9074 - 1) / 0.0374
 2 * pnorm(t_stat)
-Acf(w_sox_ts)
 # NOT a random walk
+
+# White Sox Acf and differenced Acf
+Acf(w_sox_train)
+Acf(diff(w_sox_train))
 
 # Plot
 autoplot(w_sox_train) +
@@ -207,13 +227,13 @@ autoplot(w_sox_ts) +
   autolayer(lm_t_w_sox_forecast, series = 'Trend', PI = FALSE, linetype = 'dashed') +
   autolayer(lm_p_w_sox_forecast, series = 'Polynomial', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('White Sox Attendance Model (2020-2024)') + 
+  ggtitle('White Sox Naive/Trend Attendance Model (2022-2024)') + 
   theme_minimal()
 #ggsave('vizzes/w_sox_naive_trend.jpeg', width = 10, height = 6, dpi = 300)
 
 # MA AND HOLT
 # MA model
-ma_w_sox <- rollmean(w_sox_train, k = 5, align = 'right')
+ma_w_sox <- rollmean(w_sox_train, k = 3, align = 'right')
 ma_w_sox_last <- tail(ma_w_sox, 1)
 ma_w_sox_forecast <- ts(rep(ma_w_sox_last, nVal), start = c(1903, nTrain + 1), end = c(1903, nTrain + nVal),
                         frequency = 1)
@@ -225,7 +245,6 @@ holt_w_sox_forecast <- forecast(holt_w_sox, h = nVal)
 # Check accuracy
 accuracy(ma_w_sox_forecast, w_sox_val)
 accuracy(holt_w_sox_forecast, w_sox_val)
-accuracy(ets_w_sox_forecast, w_sox_val)
 
 # Plot
 autoplot(w_sox_ts) +
@@ -235,26 +254,27 @@ autoplot(w_sox_ts) +
   autolayer(ma_w_sox_forecast, series = 'MA', PI = FALSE, linetype = 'dashed') +
   autolayer(holt_w_sox_forecast, series = 'Holt', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('White Sox Attendance') + 
+  ggtitle('White Sox MA and Holt Attendance Model (2022-2024)') + 
   theme_minimal()
+#ggsave('vizzes/w_sox_ma_holt.jpeg', width = 10, height = 6, dpi = 300)
 
 # ARIMA MODELS
 # Auto ARIMA first
 auto_arima_w_sox <- auto.arima(w_sox_train)
 auto_arima_w_sox_forecast <- forecast(auto_arima_w_sox, h = nVal)
 
-# Sarimax with wins
-sarimax_w_sox <- auto.arima(w_sox_train, xreg = w_sox$wins[1:length(w_sox_train)])
-sarimax_w_sox_forecast <- forecast(sarimax_w_sox, xreg = w_sox$wins[(length(w_sox_train) + 1):(length(w_sox_train) + nVal)])
+# Arimax with wins
+arimax_w_sox <- auto.arima(w_sox_train, xreg = w_sox$wins[1:length(w_sox_train)])
+arimax_w_sox_forecast <- forecast(arimax_w_sox, xreg = w_sox$wins[(length(w_sox_train) + 1):(length(w_sox_train) + nVal)])
 
-# Sarimax with lagged wins
-sarimax_w_sox_lag <- auto.arima(w_sox_train, xreg = w_sox$wins_lag[1:length(w_sox_train)])
-sarimax_w_sox_lag_forecast <- forecast(sarimax_w_sox_lag, xreg = w_sox$wins_lag[(length(w_sox_train) + 1):(length(w_sox_train) + nVal)])
+# Arimax with lagged wins
+arimax_w_sox_lag <- auto.arima(w_sox_train, xreg = w_sox$wins_lag[1:length(w_sox_train)])
+arimax_w_sox_lag_forecast <- forecast(arimax_w_sox_lag, xreg = w_sox$wins_lag[(length(w_sox_train) + 1):(length(w_sox_train) + nVal)])
 
 # Check accuracies
 accuracy(auto_arima_w_sox_forecast, w_sox_val)
-accuracy(sarimax_w_sox_forecast, w_sox_val)
-accuracy(sarimax_w_sox_lag_forecast, w_sox_val)
+accuracy(arimax_w_sox_forecast, w_sox_val)
+accuracy(arimax_w_sox_lag_forecast, w_sox_val)
 
 # Plot
 autoplot(w_sox_ts) +
@@ -262,11 +282,12 @@ autoplot(w_sox_ts) +
   autolayer(w_sox_val, series = 'Validation') +
   autolayer(naive_w_sox_forecast, series = 'Naive', PI = FALSE, linetype = 'dashed') +
   autolayer(auto_arima_w_sox_forecast, series = 'Auto ARIMA', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_w_sox_forecast, series = 'Sarimax', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_w_sox_lag_forecast, series = 'Sarimax Lagged', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_w_sox_forecast, series = 'Arimax', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_w_sox_lag_forecast, series = 'Arimax Lagged', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('White Sox Attendance') + 
+  ggtitle('White Sox ARIMA Attendance Model (2022-2024)') + 
   theme_minimal()
+#ggsave('vizzes/w_sox_arima.jpeg', width = 10, height = 6, dpi = 300)
 
 # Model Deployment
 # Predict
@@ -275,19 +296,19 @@ auto_arima_w_sox_future <- auto.arima(w_sox_ts, xreg = w_sox$wins_lag)
 # Fit simple linear model to estimate future wins
 win_years <- seq_along(w_sox$wins)
 w_sox_win_model <- lm(w_sox$wins ~ win_years)
-w_sox_future_years <- (max(win_years) + 1):(max(win_years) + 6)
+w_sox_future_years <- (max(win_years) + 1):(max(win_years) + 4)
 w_sox_future_wins <- predict(w_sox_win_model, newdata = data.frame(win_years = w_sox_future_years))
 # Lag future wins
 w_sox_future_wins_lag <- lag(w_sox_future_wins, 1)
 
 # Forecast
-auto_arima_w_sox_forecast <- forecast(auto_arima_w_sox_future, xreg = na.omit(w_sox_future_wins_lag), h = 5)
+arimax_w_sox_forecast <- forecast(arimax_w_sox_lag, xreg = na.omit(w_sox_future_wins_lag), h = 5)
 
 # Plot
 autoplot(w_sox_ts) +
-  autolayer(auto_arima_w_sox_forecast) +
+  autolayer(arimax_w_sox_forecast) +
   xlab('Year') + ylab('Attendance') +
-  ggtitle('White Sox Attendance Forecast: SARIMAX with Lagged Wins') +
+  ggtitle('White Sox Attendance Forecast: SARIMAX with Lagged Wins (2025-2027)') +
   theme_minimal()
 #ggsave('vizzes/w_sox_forecast.jpeg', width = 10, height = 6, dpi = 300)
 
@@ -302,8 +323,11 @@ rw_tigers <- Arima(tigers_train, order = c(1, 0, 0))
 summary(rw_tigers)
 t_stat <- (0.8570 - 1) / 0.0528
 2 * pnorm(t_stat)
-Acf(tigers_train)
 # NOT a random walk
+
+# Acf of Tigers and differenced
+Acf(tigers_train)
+Acf(diff(tigers_train))
 
 # Plot it out
 autoplot(tigers_ts) +
@@ -338,12 +362,13 @@ autoplot(tigers_ts) +
   autolayer(naive_tigers_forecast, series = 'Naive', PI = FALSE, linetype = 'dashed') +
   autolayer(lm_t_tigers_forecast, series = 'Trend', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('Tigers Attendance') + 
+  ggtitle('Tigers Naive/Trend Attendance Model (2022-2024)') + 
   theme_minimal()
+#ggsave('vizzes/tigers_naive_trend.jpeg', width = 10, height = 6, dpi = 300)
 
 # MA and Holt
 # MA model
-ma_tigers <- rollmean(tigers_train, k = 5, align = 'right')
+ma_tigers <- rollmean(tigers_train, k = 3, align = 'right')
 ma_tigers_last <- tail(ma_tigers, 1)
 ma_tigers_forecast <- ts(rep(ma_tigers_last, nVal), start = c(1903, nTrain + 1), end = c(1903, nTrain + nVal),
                         frequency = 1)
@@ -364,7 +389,7 @@ autoplot(tigers_ts) +
   autolayer(ma_tigers_forecast, series = 'MA', PI = FALSE, linetype = 'dashed') +
   autolayer(holt_tigers_forecast, series = 'Holt', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('Tigers Attendance Model (2020-2024)') + 
+  ggtitle('Tigers MA/Holt Attendance Model (2020-2024)') + 
   theme_minimal()
 #ggsave('vizzes/tigers_naive_ma.jpeg', width = 10, height = 6, dpi = 300)
 
@@ -373,18 +398,18 @@ autoplot(tigers_ts) +
 auto_arima_tigers <- auto.arima(tigers_train)
 auto_arima_tigers_forecast <- forecast(auto_arima_tigers, h = nVal)
 
-# Sarimax
-sarimax_tigers <- auto.arima(tigers_train, xreg = tigers$wins[1:length(tigers_train)])
-sarimax_tigers_forecast <- forecast(sarimax_tigers, xreg = tigers$wins[(length(tigers_train) + 1):(length(tigers_train) + nVal)])
+# Arimax
+arimax_tigers <- auto.arima(tigers_train, xreg = tigers$wins[1:length(tigers_train)])
+arimax_tigers_forecast <- forecast(arimax_tigers, xreg = tigers$wins[(length(tigers_train) + 1):(length(tigers_train) + nVal)])
 
-# Sarimax with lagged wins
-sarimax_tigers_lag <- auto.arima(tigers_train, xreg = tigers$wins_lag[1:length(tigers_train)])
-sarimax_tigers_lag_forecast <- forecast(sarimax_tigers_lag, xreg = tigers$wins_lag[(length(tigers_train) + 1):(length(tigers_train) + nVal)])
+# Arimax with lagged wins
+arimax_tigers_lag <- auto.arima(tigers_train, xreg = tigers$wins_lag[1:length(tigers_train)])
+arimax_tigers_lag_forecast <- forecast(arimax_tigers_lag, xreg = tigers$wins_lag[(length(tigers_train) + 1):(length(tigers_train) + nVal)])
 
 # Check accuracies
 accuracy(auto_arima_tigers_forecast, tigers_val)
-accuracy(sarimax_tigers_forecast, tigers_val)
-accuracy(sarimax_tigers_lag_forecast, tigers_val)
+accuracy(arimax_tigers_forecast, tigers_val)
+accuracy(arimax_tigers_lag_forecast, tigers_val)
 
 # Plot
 autoplot(tigers_ts) +
@@ -392,23 +417,27 @@ autoplot(tigers_ts) +
   autolayer(tigers_val, series = 'Validation') +
   autolayer(naive_tigers_forecast, series = 'Naive', PI = FALSE, linetype = 'dashed') +
   autolayer(auto_arima_tigers_forecast, series = 'Auto ARIMA', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_tigers_forecast, series = 'Sarimax', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_tigers_lag_forecast, series = 'Sarimax Lagged', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_tigers_forecast, series = 'Sarimax', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_tigers_lag_forecast, series = 'Sarimax Lagged', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('Tigers Attendance') + 
+  ggtitle('Tigers ARIMA Attendance Model (2022-2024)') + 
   theme_minimal()
+#ggsave('vizzes/tigers_arima.jpeg', width = 10, height = 6, dpi = 300)
 
-# Predict with Holt
-holt_tigers_future <- ets(tigers_ts, model = 'AAN')
-holt_tigers_forecast <- forecast(holt_tigers_future, h = 5)
+# Predict with MA
+# MA assumes simple average continues
+ma_tigers_future <- rollmean(tigers_ts, k = 3, align = 'right')
+ma_tigers_last <- tail(ma_tigers_future, 1)
+ma_tigers_forecast <- rep(ma_tigers_last, 3)
+ma_tigers_forecast_ts <- ts(ma_tigers_forecast, start = 2025, end = 2027)
 
 # Plot
 autoplot(tigers_ts) +
-  autolayer(holt_tigers_forecast) +
+  autolayer(ma_tigers_forecast_ts, series = 'Forecast') +
   xlab('Year') + ylab('Attendance') +
-  ggtitle("Tigers Attendance Forecast: Holt's Method") +
+  ggtitle("Tigers Attendance Forecast: Moving Average Method") +
   theme_minimal()
-ggsave('vizzes/tigers_forecast.jpeg', width = 10, height = 6, dpi = 300)
+#ggsave('vizzes/tigers_forecast.jpeg', width = 10, height = 6, dpi = 300)
 
 # PIRATES MODEL
 # Create time series object
@@ -421,8 +450,11 @@ rw_pirates <- Arima(pirates_train, order = c(1, 0, 0))
 summary(rw_pirates)
 t_stat <- (0.8389 - 1) / 0.0548
 2 * pnorm(t_stat)
-Acf(pirates_train)
 # NOT a random walk
+
+# Acf of Pirates and differenced
+Acf(pirates_train)
+Acf(diff(pirates_train))
 
 # Plot it out
 autoplot(pirates_ts) +
@@ -457,12 +489,13 @@ autoplot(pirates_ts) +
   autolayer(naive_pirates_forecast, series = 'Naive', PI = FALSE, linetype = 'dashed') +
   autolayer(lm_t_pirates_forecast, series = 'Trend', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('Pirates Attendance') + 
+  ggtitle('Pirates Naive/Trend Attendance Model (2022-2024)') + 
   theme_minimal()
+#ggsave('vizzes/pirates_naive_trend.jpeg', width = 10, height = 6, dpi = 300)
 
 # MA and Holt
 # MA model
-ma_pirates <- rollmean(pirates_train, k = 5, align = 'right')
+ma_pirates <- rollmean(pirates_train, k = 3, align = 'right')
 ma_pirates_last <- tail(ma_pirates, 1)
 ma_pirates_forecast <- ts(rep(ma_pirates_last, nVal), start = c(1903, nTrain + 1), end = c(1903, nTrain + nVal),
                         frequency = 1)
@@ -483,26 +516,27 @@ autoplot(pirates_ts) +
   autolayer(ma_pirates_forecast, series = 'MA', PI = FALSE, linetype = 'dashed') +
   autolayer(holt_pirates_forecast, series = 'Holt', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('Pirates Attendance') + 
+  ggtitle('Pirates MA/Holt Attendance Model (2022-2024)') + 
   theme_minimal()
+#ggsave('vizzes/pirates_ma_holt.jpeg', width = 10, height = 6, dpi = 300)
 
 # ARIMA MODELS
 # Auto ARIMA first
 auto_arima_pirates <- auto.arima(pirates_train)
 auto_arima_pirates_forecast <- forecast(auto_arima_pirates, h = nVal)
 
-# Sarimax
-sarimax_pirates <- auto.arima(pirates_train, xreg = pirates$wins[1:length(pirates_train)])
-sarimax_pirates_forecast <- forecast(sarimax_pirates, xreg = pirates$wins[(length(pirates_train) + 1):(length(pirates_train) + nVal)])
+# Arimax
+arimax_pirates <- auto.arima(pirates_train, xreg = pirates$wins[1:length(pirates_train)])
+arimax_pirates_forecast <- forecast(arimax_pirates, xreg = pirates$wins[(length(pirates_train) + 1):(length(pirates_train) + nVal)])
 
-# Sarimax with lagged wins
-sarimax_pirates_lag <- auto.arima(pirates_train, xreg = pirates$wins_lag[1:length(pirates_train)])
-sarimax_pirates_lag_forecast <- forecast(sarimax_pirates_lag, xreg = pirates$wins_lag[(length(pirates_train) + 1):(length(pirates_train) + nVal)])
+# Arimax with lagged wins
+arimax_pirates_lag <- auto.arima(pirates_train, xreg = pirates$wins_lag[1:length(pirates_train)])
+arimax_pirates_lag_forecast <- forecast(arimax_pirates_lag, xreg = pirates$wins_lag[(length(pirates_train) + 1):(length(pirates_train) + nVal)])
 
 # Check accuracies
 accuracy(auto_arima_pirates_forecast, pirates_val)
-accuracy(sarimax_pirates_forecast, pirates_val)
-accuracy(sarimax_pirates_lag_forecast, pirates_val)
+accuracy(arimax_pirates_forecast, pirates_val)
+accuracy(arimax_pirates_lag_forecast, pirates_val)
 
 # Plot
 autoplot(pirates_ts) +
@@ -510,28 +544,25 @@ autoplot(pirates_ts) +
   autolayer(pirates_val, series = 'Validation') +
   autolayer(naive_pirates_forecast, series = 'Naive', PI = FALSE, linetype = 'dashed') +
   autolayer(auto_arima_pirates_forecast, series = 'Auto ARIMA', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_pirates_forecast, series = 'Sarimax', PI = FALSE, linetype = 'dashed') +
-  autolayer(sarimax_pirates_lag_forecast, series = 'Sarimax Lagged', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_pirates_forecast, series = 'Sarimax', PI = FALSE, linetype = 'dashed') +
+  autolayer(arimax_pirates_lag_forecast, series = 'Sarimax Lagged', PI = FALSE, linetype = 'dashed') +
   xlab('Year') + ylab('Attendance') + 
-  ggtitle('Pirates Attendance Model (2020-2024)') + 
+  ggtitle('Pirates ARIMA Attendance Model (2022-2024)') + 
   theme_minimal()
 #ggsave('vizzes/pirates_arima.jpeg', width = 10, height = 6, dpi = 300)
 
-# Predict
-sarimax_pirates_future <- auto.arima(pirates_ts, xreg = pirates$wins)
-
-# Fit simple linear model to estimate future wins
-win_years <- seq_along(pirates$wins)
-pirates_win_model <- lm(pirates$wins ~ win_years)
-pirates_future_years <- (max(win_years) + 1):(max(win_years) + 5)
-pirates_future_wins <- predict(pirates_win_model, newdata = data.frame(win_years = pirates_future_years))
-sarimax_pirates_forecast <- forecast(sarimax_pirates_future, xreg = pirates_future_wins, h = 5)
+# Predict with MA
+# MA assumes simple average continues
+ma_pirates_future <- rollmean(pirates_ts, k = 3, align = 'right')
+ma_pirates_last <- tail(ma_pirates_future, 1)
+ma_pirates_forecast <- rep(ma_pirates_last, 3)
+ma_pirates_forecast_ts <- ts(ma_pirates_forecast, start = 2025, end = 2027)
 
 # Plot
 autoplot(pirates_ts) +
-  autolayer(sarimax_pirates_forecast) +
+  autolayer(ma_pirates_forecast_ts, series = 'Forecast') +
   xlab('Year') + ylab('Attendance') +
-  ggtitle('Pirates Attendance Forecast: SARIMAX with Wins') +
+  ggtitle("Pirates Attendance Forecast: Moving Average Method") +
   theme_minimal()
 #ggsave('vizzes/pirates_forecast.jpeg', width = 10, height = 6, dpi = 300)
 
